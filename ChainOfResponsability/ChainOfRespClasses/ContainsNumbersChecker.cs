ae2eb35cf157c97a971ca79f818bb5ef66e75f03
Alessandro Ferluga <alessandroferluga97@gmail.com>
2023-05-02 12:29:57 +0200

    Add Chain of Responsability Solution

---
+++ ChainOfResponsability/ChainOfRespClasses/ContainsNumbersChecker.cs
@@ -0,0 +1,20 @@
+﻿namespace ChainOfRespClasses;
+
+public class ContainsNumbersChecker : Checker
+{
+    public ContainsNumbersChecker(Checker checker) : base(checker)
+    {
+
+    }
+
+    public override (bool, string) Check(List<string> value)
+    {
+        if (value.All(p => string.IsNullOrWhiteSpace(p)))
+            return (false, "String is empty");
+
+        var listOfNumbers = value.Where(p => int.TryParse(p, out _)).ToList();
+        if (_nextChecker is not null)
+            return _nextChecker.Check(listOfNumbers);
+        return (true, "Valid list");
+    }
+}
---
+++ ChainOfResponsability/ChainOfRespClasses/IntegerNumberChecker.cs
@@ -0,0 +1,21 @@
+﻿namespace ChainOfRespClasses;
+
+public class IntegerNumberChecker : Checker
+{
+    public IntegerNumberChecker(Checker? nextChecker = null) : base(nextChecker)
+    {
+
+    }
+
+    public override (bool, string) Check(List<string> values)
+    {
+
+
+        var hasNegatives = values.Select(p => int.Parse(p)).Any(p => p < 0);
+        if (hasNegatives)
+            return (false, "string contains negative numbers");
+        if (_nextChecker is not null)
+            return _nextChecker.Check(values);
+        return (true, "string is valid");
+    }
+}
